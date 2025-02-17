75739b135e1b33f50478e1c9df9f6a1f7002bf2a
MaheninaRan <120193477+MaheninaRan@users.noreply.github.com>
2025-02-17 10:32:59 +0300

    lasa

---
+++ data.php
@@ -0,0 +1,14 @@
+<?php 
+   $words = [
+       "ordinateur",
+       "clavier",
+       "souris",
+       "écran",
+       "internet",
+       "serveur",
+       "logiciel",
+       "base de données",
+       "programmation",
+       "algorithme"
+   ];   
+?>
